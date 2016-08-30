3f7252e09f4a29d7c81d59f6592503e88210283d
qq783175223 <783175223@qq.com>
2016-08-30 11:19:25 +0800

    矩形覆盖

---
+++ RectCover.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int RectCover(int target) {
+		if(target==0){
+            return 0;
+        }
+        if(target==1){
+            return 1;
+        }
+        if(target==2){
+            return 2;
+        }
+        int last=2,lastlast=1,cnt=target-2,tmp;
+        while(cnt-->0){
+            tmp=last;
+            last +=lastlast;
+            lastlast=tmp;
+        }
+        return last;
+    }
+}
