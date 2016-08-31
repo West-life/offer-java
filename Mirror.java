61d9cdbd2eb43e4b88484fcc31de52fc022b30fc
qq783175223 <783175223@qq.com>
2016-08-31 10:49:02 +0800

    二叉树的镜像

---
+++ Mirror.java
@@ -0,0 +1,28 @@
+/**
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
+public class Solution {
+    public void Mirror(TreeNode root) {
+        if(root==null){
+            return;
+        }
+        TreeNode tmp=null;
+        tmp=root.left;
+        root.left=root.right;
+        root.right=tmp;
+        //先递归左还是先右都可以
+        Mirror(root.right);
+        Mirror(root.left);
+        
+    }
+}
