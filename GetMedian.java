252601c1b6a0f8c69e8d0852b58ba60ccbaae500
qq783175223 <783175223@qq.com>
2016-09-07 21:21:01 +0800

    数据流中的中位数

---
+++ GetMedian.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Solution {
+    private Comparator<Integer> revCmp=new Comparator<Integer>(){
+        public int compare(Integer num1,Integer num2){
+            return num2.compareTo(num1);
+        }
+    };
+    private PriorityQueue<Integer> minHeap=new PriorityQueue<Integer>();
+    private PriorityQueue<Integer> maxHeap=new PriorityQueue<Integer>(15,revCmp);
+    private int elementNum=0;
+    public void Insert(Integer num) {
+    	if(elementNum%2==0){
+            minHeap.add(num);
+        }else{
+            maxHeap.add(num);
+        }
+        elementNum++;
+        if(!maxHeap.isEmpty()&&maxHeap.peek()>minHeap.peek()){
+            minHeap.add(maxHeap.poll());
+            maxHeap.add(minHeap.poll());
+        }
+    }
+
+    public Double GetMedian() {
+        if(!maxHeap.isEmpty()&&elementNum%2==0){
+            return new Double((maxHeap.peek()+minHeap.peek()))/2;
+        }else{
+            return new Double(minHeap.peek());
+        }
+    }
+}
