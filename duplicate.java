98b167d2d732be7326c5e30116a766eca358d80d
qq783175223 <783175223@qq.com>
2016-09-16 15:39:35 +0800

    数组中重复的数字

--- duplicate.java
+++ duplicate.java
@@ -1,4 +1,40 @@
-//方法一：
+//方法三：还是书上这个方法最好，时间O（n），空间O（1）
+public class Solution {
+    // Parameters:
+    //    numbers:     an array of integers
+    //    length:      the length of array numbers
+    //    duplication: (Output) the duplicated number in the array number,length of duplication array is 1,so using duplication[0] = ? in implementation;
+    //                  Here duplication like pointor in C/C++, duplication[0] equal *duplication in C/C++
+    //    这里要特别注意~返回任意重复的一个，赋值duplication[0]
+    // Return value:       true if the input is valid, and there are some duplications in the array number
+    //                     otherwise false
+    public boolean duplicate(int numbers[],int length,int [] duplication) {
+        if(numbers==null||length<=0){
+            return false;
+        }
+        for(int i=0;i<length;i++){
+            if(numbers[i]<0||numbers[i]>length-1){
+                return false;
+            }
+        }
+        for(int i=0;i<length;i++){
+            while(numbers[i]!=i){
+                if(numbers[i]==numbers[numbers[i]]){
+                    duplication[0]=numbers[i];
+                    return true;
+                }
+                int tmp=numbers[i];
+                numbers[i]=numbers[tmp];
+                numbers[tmp]=tmp;
+            }
+        }
+        return false;
+    }
+}
+
+
+
+//方法一：时间O（n^2），空间O（1）
 public class Solution {
     // Parameters:
     //    numbers:     an array of integers
@@ -23,7 +59,7 @@ public class Solution {
     }
 }
 
-//方法二：
+//方法二：这个方法有限制，当元素值很大时，int类型相加可能会超过范围
 
 public class Solution {
     // Parameters:
