893766b29b5558cec86143b85e8ae79074cec057
qq783175223 <783175223@qq.com>
2016-09-17 14:48:53 +0800

    斐波那契数列

---
+++ Fibonacci.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int Fibonacci(int n) {
+        if(n<1){
+            return 0;
+        }
+        if(n<=2){
+            return 1;
+        }
+        int result=0,f0=1,f1=1,tmp;
+        for(int i=2;i<n;i++){
+            result=f0+f1;
+            tmp=f1;
+            f1=result;
+            f0=tmp;
+        }
+        return result;
+    }
+}
