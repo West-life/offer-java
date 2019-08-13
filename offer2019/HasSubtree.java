74e4a3ee7a16a823886dcf6eaf7a52a72e61b178
xuzhijvn <783175223@qq.com>
2019-08-13 08:59:44 +0800

    树的子结构

--- offer2019/HasSubtree.java
+++ offer2019/HasSubtree.java
@@ -3,6 +3,20 @@ package offer2019;
 /**
  * Create by xuzhijun.online on 2019/8/13.
  */
+
+/**
+ public class TreeNode {
+ int val = 0;
+ TreeNode left = null;
+ TreeNode right = null;
+
+ public TreeNode(int val) {
+ this.val = val;
+
+ }
+
+ }
+ */
 public class HasSubtree {
     public boolean HasSubtree(TreeNode root1, TreeNode root2) {
         if (root1 == null || root2 == null) {
